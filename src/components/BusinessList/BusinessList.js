f1411b227e8a988f9dfe1e8b50f7f08cb18f6547
stelian nechita <steliannechita2013@gmail.com>
2019-08-26 12:16:25 +0100

    refactored

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -16,7 +16,7 @@ class BusinessList extends React.Component {
       return (
         <h3 id="noResults">
           {this.props.errors.description ||
-            "Couldn't perform the search. Please check your internet connection"}
+            "Couldn't perform the search. Please check your internet connection and reload the page"}
         </h3>
       );
     }
